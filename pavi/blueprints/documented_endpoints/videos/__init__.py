bc803222e648f3528fd68ad0d91accda20024e21
Hector Cosgalla <valcos98@hotmail.com>
2021-10-27 21:23:20 -0500

    Post workinggit add .

--- pavi/blueprints/documented_endpoints/videos/__init__.py
+++ pavi/blueprints/documented_endpoints/videos/__init__.py
@@ -2,12 +2,15 @@ from flask import request
 from flask_restplus import Namespace, Resource, fields, reqparse, Api
 from werkzeug.datastructures import FileStorage
 from http import HTTPStatus
+
+from pavi.config import Config
+from pavi.util.process_video_utils import save_uploaded_video
 from pavi.util.service_utils import send_to_service
 
 namespace = Namespace('videos', 'Video processing endpoint')
 
 upload_parser = namespace.parser()
-upload_parser.add_argument('algorithm', location='form', type='string', required=True, default='yolov4')
+upload_parser.add_argument('algorithm', location='form', required=True, default='yolov4')
 upload_parser.add_argument('video', location='files', type=FileStorage, required=True)
                            
 video_model = namespace.model('videoid', {
@@ -45,13 +48,12 @@ class entities(Resource):
 
     @namespace.response(400, 'Entity with the given name already exists')
     @namespace.response(500, 'Internal Server error')
+    @namespace.response(201, 'Video uploaded sucessfully')
     @namespace.expect(upload_parser)
     def post(self):
         '''Añade y procesa un video en la plataforma'''
         args = upload_parser.parse_args()
-        video_path = args['video']
+        video_path = save_uploaded_video(args['video'], Config.get('upload_folder'))
         algo = args['algorithm']
-        print(video_path)
-        print(algo)
         url = send_to_service(algo, video_path)
         return {'url': url}, 201
